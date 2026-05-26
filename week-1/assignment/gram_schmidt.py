f631c56783d360b69420a1f8794981981f9a58c2
challasivaramya <challasivaramya@gmail.com>
2026-05-26 21:45:01 +0530

    Add files via upload

---
+++ week-1/assignment/gram_schmidt.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def gram_schmidt(A: np.ndarray) -> np.ndarray:
+    A=A.T
+    U=np.zeros_like(A,dtype=float)
+    n=A.shape[0]
+    for i in range(n):
+        v=A[i]
+        if i>0:
+            s=np.sum(((np.sum(v*U[:i],axis=1)/np.sum(U[:i]*U[:i],axis=1)).reshape(-1,1))*U[:i],axis=0)
+            U[i]=v-s
+        else:
+            U[i]=v
+    norm=np.linalg.norm(U,axis=1)
+    Q=(U/norm[:,None]).T
+    return Q
